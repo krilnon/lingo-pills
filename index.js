5ea5dc19d0ad8e6eb376475c9fa0fca104ad76b7
Alexandre Alexandre <alexandre67fr@gmail.com>
2013-06-25 17:27:37 +0200

    Fixing the issues

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var
-	terms = ['TPS report', 'fax', '  auto', '  automatic', '  autocomplete', 'autocompletions']
+	terms = ['TPS report', 'fax', '  auto', '  automatic', '  autocomplete', 'autocompletions'];
 
-$(init)
+$(init);
+
+function insertNodeAtCursor(node) {
+    var sel, range, html;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode( node );
+        }
+    } else if (document.selection && document.selection.createRange) {
+        //document.selection.createRange().text = text;
+    }
+}
 
 function init(){
-	$('#text-area').focus()
+	$('#text-area').focus();
 	displayTerms();
 
 	$('body').on('focus', '[contenteditable]', function() {
@@ -25,20 +39,122 @@ function init(){
 	$('[contenteditable]').bind('change', function() {
 		checkTerms(this);
 	});	
+  
+	$('[contenteditable]').keypress(function(e) {
+      // only @ and + characters
+		if ( (e.keyCode != 64) && (e.keyCode != 43) ) 
+        return;
+      e.preventDefault();
+      var element = this;
+      var node =
+          $("<input />")
+             .val('')
+             .css({
+               border: '0px',
+               background: 'transparent',
+               outline: 'none',
+               margin: 0,
+               padding: '0 0 0 0',
+               lineHeight: $(element).css('line-height'),
+               fontSize: $(element).css('font-size'),
+               display: 'inline',
+               verticalAlign: 'baseline',
+               width: 'auto',
+               color: $(element).css('color'),
+               minWidth: '10px'
+             })
+             .attr("tabindex", -1)
+             .attr("contenteditable", false)
+      ;
+      insertNodeAtCursor(node[0]);
+     
+      element.isAutoCompleting = true;
+      function stopEditing() {
+        element.isAutoCompleting = false;
+        node.before( node.val() );
+        node.remove();
+        checkTerms(element);
+      }
+      
+      // get width
+      function resizeInput() {
+         var span = $("<span />");
+         var escaped = node.val().replace(/&/g, '&amp;').replace(/\s/g,' ').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+         span.html(escaped);
+         span.insertAfter(node);
+         var exactW = span.width();
+         exactW += 10;
+         node.css('width', exactW+'px');
+         span.remove();
+      }
+     
+      node
+         .blur(stopEditing)
+         .keypress(function (event) {
+           if ( (event.keyCode != 13) )
+             return;
+           event.preventDefault();
+           stopEditing();
+           return false;
+         })
+         .keyup(resizeInput)
+         .keydown(resizeInput)
+         .autocomplete({
+            source: terms,
+            minLength: 0,
+            position: {
+              //collision: "flipfit"
+              my: "left top",
+              at: "left bottom",
+              collision: "fit flip"
+            },
+            select: function( event, ui ) {
+              //console.log(event, ui);
+              $(this).val( ui.item.value );
+              stopEditing();
+            },
+            close: function( event ) {
+              var ev = event.originalEvent;
+              if (!ev) 
+                stopEditing();
+              if ( ev.type === "keydown" && ev.keyCode === $.ui.keyCode.ESCAPE ) {
+                stopEditing();
+              }
+            }
+         })
+         .focus(function () {
+           resizeInput();
+           if (this.value === "")
+              $(this).autocomplete("search");
+         })
+         .focus()
+      ;
+     
+      resizeInput();
+      return false;
+	});	
+  
 }
 
 function displayTerms(){
-	var $termList = $('<ul />', { id: 'term-list' })
+	var $termList = $('<ul />', { id: 'term-list' });
+   terms = $.map(terms, function(s, i){
+      return s.replace(/(^ +| +$)/, '');
+   });
 	$.each(terms, function(i, term){
-		$termItem = $('<li />', { text: term })
-		$termList.append($termItem)
-	})
+		$termItem = $('<li />', { text: term });
+		$termList.append($termItem);
+	});
 	
-	$('#term-area').append($termList)
+	$('#term-area').append($termList);
 }
 
 // check text-area content for terms
 function checkTerms(editableObj) {
+  
+    if (editableObj.isAutoCompleting)
+      return;
+  
     var savedSel;
     // store current sel
     if (rangy.hasOwnProperty('saveSelection'))
@@ -53,10 +169,13 @@ function checkTerms(editableObj) {
 
 function highlightTerms(element) {
     
+    if (element.isAutoCompleting)
+      return;
+  
     for (var i = 0, childCounts = element.childNodes.length; i < childCounts; i++) {
 
         var child = element.childNodes[i];
-        console.log(child.nodeName + " :" + child.nodeType);
+        //console.log(child.nodeName + " :" + child.nodeType);
         if (child.nodeType == 1) { // container
 
             if (child.nodeName == 'BUTTON') // skip button
@@ -64,9 +183,16 @@ function highlightTerms(element) {
             highlightTerms(child);
         } else if (child.nodeType == 3) { // text element
 
-            for (var j = 0; j < terms.length; j++) 
+            var _terms = terms.slice(0);
+            _terms.sort(function (a, b){
+              var aName = a.length;
+              var bName = b.length; 
+              return ((aName < bName) ? 1 : ((aName > bName) ? -1 : 0));
+            });
+          
+            for (var j = 0; j < _terms.length; j++) 
             {
-                var term = terms[j];
+                var term = _terms[j];
                 var index = child.data.indexOf(term);
                 if (index >= 0) {
                     var newTerm = document.createElement('button');
